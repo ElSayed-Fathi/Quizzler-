eb6a2f1e612e0a8a4129bb40fcc63e4400942ad9
El-Sayed Fathi <fthyalsyd855@gmail.com>
2021-10-06 17:29:45 +0200

    My fourth app in flutter

---
+++ lib/question.dart
@@ -0,0 +1,15 @@
+class Question {
+  // fields
+  String questionText;
+
+  bool questionAnswer;
+
+  // // constructor
+  // Question(String q, bool a) {
+  //   questionText = q;
+  //   questionAnswer = a;
+  // }
+// another constructor style
+Question (this.questionText , this.questionAnswer);
+
+}
